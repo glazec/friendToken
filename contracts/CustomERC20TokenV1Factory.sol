8e0342370771996f16bcde2f2efb0d78245bc0fe
glaze <luyiping1011@gmail.com>
2021-01-08 22:45:48 -0800

    add createToken event
    fix typo
    
    fix invalid type

--- contracts/CustomERC20TokenV1Factory.sol
+++ contracts/CustomERC20TokenV1Factory.sol
@@ -4,12 +4,20 @@ pragma solidity ^0.7.0;
 import "./CustomERC20TokenV1.sol";
 
 contract CustomERC20TokenV1Factory {
+    event CreateToken(
+        address from,
+        address tokenAddr,
+        string name,
+        string symbol
+    );
+
     function createToken(string memory name, string memory symbol)
         public
         returns (address)
     {
         CustomERC20TokenV1 token = new CustomERC20TokenV1(0, name, symbol);
         token.transferOwnership(msg.sender);
+        emit CreateToken(msg.sender, address(token), name, symbol);
         return address(token);
     }
 }
